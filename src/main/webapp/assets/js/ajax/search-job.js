d3ba2581a69639be83cfc73e5f5e9983d434de2d
Mokoghost <mokoghost@gmail.com>
2020-10-15 23:39:28 +0800

    search job jump

--- src/main/webapp/assets/js/ajax/search-job.js
+++ src/main/webapp/assets/js/ajax/search-job.js
@@ -14,8 +14,9 @@ $(function () {
             dataType: "json",
             async: false,
             success: function (msg) {
-                if(msg.message==="成功找到相关项目"){
-                    for (let job in msg.joblist) {
+                if (msg.message === "成功找到相关项目") {
+                    for (let i = 0; i < msg.joblist.length; i++) {
+                        let job = msg.joblist[i];
                         $('#jobs').append('                <div class="col-lg-4 col-md-6 col-sm-6">\n' +
                             '                    <div class="classic-joblist ultimate-center">\n' +
                             '                        <div class="cl-job-employer">\n' +
@@ -23,14 +24,14 @@ $(function () {
                             '                                <a href="job-detail.html"><img src="assets/img/软件学院.png" alt=""/></a>\n' +
                             '                            </div>\n' +
                             '                            <div class="cljb-emp-detail">\n' +
-                            '                                <h5><a href="employer-detail.html">'+job.campus+'</a></h5>\n' +
-                            '                                <span class="cljb-date">'+job.start_time+'</span>\n' +
+                            '                                <h5><a href="employer-detail.html">' + job.campus + '</a></h5>\n' +
+                            '                                <span class="cljb-date">' + job.start_time + '</span>\n' +
                             '                            </div>\n' +
                             '                        </div>\n' +
-                            '                        <h4 class="job-title"><a href="job-detail.html">'+job.title+'</a>\n' +
+                            '                        <h4 class="job-title"><a href="job-detail.html">' + job.title + '</a>\n' +
                             '                        </h4>\n' +
                             '                        <div class="cl-job-intro">\n' +
-                            '                            <span class="cl-jb-loaction"><i class="ti-location-pin"></i>'+job.poster+'</span>\n' +
+                            '                            <span class="cl-jb-loaction"><i class="ti-location-pin"></i>' + job.poster + '</span>\n' +
                             '                        </div>\n' +
                             '                    </div>\n' +
                             '                </div>');
