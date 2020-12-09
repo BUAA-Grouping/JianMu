d19e60b12a3bc95bcc6f1c936465073345b83d81
Amadeus <13202127055@163.com>
2020-12-09 10:45:55 +0800

    apply service v1.3

--- src/main/java/com/webapp/service/SearchJobService.java
+++ src/main/java/com/webapp/service/SearchJobService.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface SearchJobService {
     int searchJob(String keyword, int college, int campus, List<Job> jobList, List<User> poster, List<Timestamp> expected_end_time);
 
+    Job getDetail(int jobId);
 }
