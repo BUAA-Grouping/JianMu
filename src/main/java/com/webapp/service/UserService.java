1e580e81d48c5a008f75a658f2fcfb25d423cc50
Amadeus <13202127055@163.com>
2020-12-09 17:15:00 +0800

    apply service v1.96

--- src/main/java/com/webapp/service/UserService.java
+++ src/main/java/com/webapp/service/UserService.java
@@ -3,8 +3,10 @@ package com.webapp.service;
 import com.webapp.pojo.Student;
 import com.webapp.pojo.User;
 
-public interface ModifyUserService {
+public interface UserService {
     User getUser(String emailId);
+
+    User getUserByUserId(int userId);
 
     boolean modify(Student user);
 }
--- src/main/java/com/webapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/webapp/service/impl/UserServiceImpl.java
@@ -4,9 +4,9 @@ import com.webapp.dao.UserDao;
 import com.webapp.dao.impl.StudentDaoImpl;
 import com.webapp.pojo.Student;
 import com.webapp.pojo.User;
-import com.webapp.service.ModifyUserService;
+import com.webapp.service.UserService;
 
-public class ModifyUserServiceImpl implements ModifyUserService {
+public class UserServiceImpl implements UserService {
     private UserDao userDao = new StudentDaoImpl();
 
     @Override
@@ -17,5 +17,9 @@ public class ModifyUserServiceImpl implements ModifyUserService {
     @Override
     public boolean modify(Student student) {
         return userDao.modify(student);
+    }
+
+    public User getUserByUserId(int userId) {
+        return userDao.queryInfoById(userId);
     }
 }
