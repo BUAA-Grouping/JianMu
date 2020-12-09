13fdb38cd48cb68d787d91fbf2cfce05df57f41a
Mokoghost <mokoghost@gmail.com>
2020-12-09 10:40:05 +0800

    前端更新

--- src/main/webapp/assets/js/ajax/quit.js
+++ src/main/webapp/assets/js/ajax/quit.js
@@ -17,22 +17,4 @@ $(function() {
             }
         });
     })
-    $("#quit2").click(function(msg) {
-        $.ajax({
-            type: "post",
-            url: "http://localhost:8080/JianMu_war/quit",
-            dataType: "json",
-            async: false,
-            success: function(msg) {
-                swal({
-                    title: msg.message,
-                    type: 'success',
-                    timer: 1000
-                });
-            },
-            error: function(xhr) {
-                swal(xhr.message, "Error!", 'error');
-            }
-        });
-    })
 })
