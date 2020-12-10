0472d24300d1628294cad841d775d4de9b88cc2e
Mokoghost <mokoghost@gmail.com>
2020-12-10 22:28:08 +0800

    搜索课程debug

--- src/main/webapp/assets/js/ajax/course-detail-display.js
+++ src/main/webapp/assets/js/ajax/course-detail-display.js
@@ -11,6 +11,7 @@ $(function () {
             let course = JSON.parse(msg.course);
             let teacher = JSON.parse(msg.teacher);
             let jobList = JSON.parse(msg.jobList);
+            let teamList = JSON.parse(msg.studentList);
             $('#capacity').text(course.capacity);
             $('#course-profile').text(course.profile);
             $('#course-title').text(course.title);
@@ -20,7 +21,7 @@ $(function () {
             $('#job-email').text(teacher.emailID);
             $('#job-telephone').text(teacher.telephone);
             $('#course-campus').text(teacher.campus);
-            addJobList(jobList);
+            addList(jobList, teamList);
         },
         error: function (xhr) {
             swal(xhr.message, "Error!", 'error');
@@ -28,6 +29,57 @@ $(function () {
     });
 })
 
-function addJobList(jobList) {
+function addJobList(jobList, teamList) {
+    for (let i = 0; i < jobList.length; i = i + 1) {
+        addJobInCourse(jobList[i], teamList[i], i);
+    }
+}
 
+function addJobInCourse(job, appliers, postJobID) {
+    let applyString = '';
+    for (let i = 0; i < appliers.length; i++) {
+        applyString += addTeammate(appliers[i]);
+    }
+    $('#job-cards').append(
+        '<div class="card">\n' +
+        '   <div class="card-header" id="heading' + postJobID + '"' + '>\n' +
+        '       <h2 class="mb-0">\n' +
+        '           <button class="btn btn-link" type="button"\n' +
+        '               data-toggle="collapse" data-target="#collapse' + postJobID + '"' + '\n' +
+        '               aria-expanded="true" aria-controls="collapse' + postJobID + '"' + '>\n' +
+        '               <div class="mg-list-wrap">\n' +
+        '                   <div class="mg-list-thumb">\n' +
+        '                       <img src="assets/img/计算机学院.png" class="mx-auto" alt=""/>\n' +
+        '                   </div>\n' +
+        '                   <div class="mg-list-caption">\n' +
+        '                       <a href="job-detail.html#' + job.id + '" class="mg-title">' + job.title + '</a>\n' +
+        '                   </div>\n' +
+        '               </div>\n' +
+        '           </button>\n' +
+        '       </h2>\n' +
+        '   </div>\n' +
+        '   <div id="collapse' + postJobID + '"' + ' class="collapse show" aria-labelledby="heading' + postJobID + '"' + '\n' +
+        '        data-parent="#accordionExample">\n' +
+        '       <div class="card-body">\n' +
+        applyString +
+        '           </div>' +
+        '       </div>\n' +
+        '   </div>\n' +
+        '</div>'
+    );
+}
+
+function addTeammate(user) {
+    return (
+        '<div class="manage-list">' +
+        '<div class="mg-list-wrap">\n' +
+        '   <div class="mg-list-thumb">\n' +
+        '       <img src="assets/img/软件学院.png" class="mx-auto" alt=""/>\n' +
+        '   </div>\n' +
+        '   <div class="mg-list-caption">\n' +
+        '       <a href="candidate-detail.html#' + user.id + '" class="mg-title">' + user.name + '</a>\n' +
+        '   </div>\n' +
+        '</div>\n' +
+        '</div>'
+    );
 }
