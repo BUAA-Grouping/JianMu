17c2bc5a7a5afe4c5e6d3c173426564981ba6204
Mokoghost <mokoghost@gmail.com>
2020-12-10 16:35:12 +0800

    老师主页

--- src/main/webapp/assets/js/ajax/modify-user-info.js
+++ src/main/webapp/assets/js/ajax/modify-user-info.js
@@ -5,21 +5,30 @@ $(function () {
         dataType: "json",
         async: false,
         success: function (msg) {
-            var object = JSON.parse(msg.userdata);
+            let object = JSON.parse(msg.userdata);
+            let type = JSON.parse(msg.type);
             $("#user-real-name").attr("value", object.name);
             $("#emailID").attr("value", object.emailID);
-            $("#schoolId").attr("value", object.studentId);
             $("#summernote").attr("value", object.profile);
             $("#user-name-big").attr("value", object.name);
-            $('#personal-index').attr("href", "candidate-detail.html#" + object.id);
             $("#user-name-big").text(object.name);
-            $("#user-greeting").text("您好！" + object.name);
+            $('#personal-index').attr("href", "candidate-detail.html#" + object.id);
             let majorNameSelect = $("#category").select2();
             majorNameSelect.val(object.collegeId.toString()).trigger("change");
             majorNameSelect.change();
             let campusNameSelect = $("#category-2").select2();
             campusNameSelect.val(object.campus.toString()).trigger("change");
             campusNameSelect.change();
+            if (type === 0) {
+                $("#schoolId").attr("value", object.studentId);
+            } else {
+                $("#manage-courses-tab").show();
+                $("#post-new-course-tab").show();
+                $("#applied-tab").hide();
+                $("#user-greeting").text("您好！" + object.name.substr(0, 1) + "老师");
+                $("#teacherId").attr("value", object.teacherId);
+                $("#teacher-title").attr("value", object.title);
+            }
         },
         error: function (xhr) {
             swal(xhr.message, "Error!", 'error');
@@ -28,18 +37,32 @@ $(function () {
     $("#save-update").click(function (message) {
         let $emailID = $("#emailID").val();
         let $username = $("#user-real-name").val();
-        let $schoolId = $("#schoolId").val();
+        let greeting = $("#user-greeting").text();
         let $profile = $("#summernote").val();
         let $major = $("#category").val();
         let $campus = $("#category-2").val();
-        let userdata = {
-            "emailID": $emailID,
-            "name": $username,
-            "studentId": $schoolId,
-            "profile": $profile,
-            "collegeId": $major,
-            "campus": $campus
-        };
+        let userdata;
+        if (greeting.substr(greeting.length - 2, 2) === "老师") {
+            let $teacherId = $("#teacherId").val();
+            userdata = {
+                "emailID": $emailID,
+                "name": $username,
+                "studentId": $teacherId,
+                "profile": $profile,
+                "collegeId": $major,
+                "campus": $campus
+            };
+        }else{
+            let $schoolId = $("#schoolId").val();
+            userdata = {
+                "emailID": $emailID,
+                "name": $username,
+                "studentId": $schoolId,
+                "profile": $profile,
+                "collegeId": $major,
+                "campus": $campus
+            };
+        }
         userdata = JSON.stringify(userdata);
         $.ajax({
             type: "post",
