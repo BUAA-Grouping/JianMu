394e1f1f0274caa52ca0a1d5ddacbc2ddc321712
Mokoghost <mokoghost@gmail.com>
2020-12-10 20:08:41 +0800

    搜索课程debug

---
+++ src/main/webapp/assets/js/ajax/join-in-course.js
@@ -0,0 +1,58 @@
+$(function () {
+    $.ajax({
+        type: "get",
+        url: "http://localhost:8080/JianMu_war/study",
+        data: {"id": courseID},
+        dataType: "json",
+        async: false,
+        success: function (msg) {
+            swal({
+                title: msg.message,
+                type: 'success',
+                timer: 1000
+            });
+            $("#join-course").hide();
+            $("#create-job").show();
+            $("#create-job").attr("",);
+        },
+        error: function (xhr) {
+            swal(xhr.message, "Error!", 'error');
+        }
+    });
+    $("#join-course").click(function () {
+        let ur = location.href;
+        let courseID = decodeURI(ur.split('#')[1]);
+        $.ajax({
+            type: "post",
+            url: "http://localhost:8080/JianMu_war/study",
+            data: {"id": courseID},
+            dataType: "json",
+            async: false,
+            success: function (msg) {
+                if (msg.message === "请先登陆"
+                    || msg.message === "无需申请自己创建的课程") {
+                    swal({
+                        title: msg.message,
+                        type: 'warning',
+                        timer: 1000
+                    });
+                } else {
+                    swal({
+                        title: msg.message,
+                        type: 'success',
+                        timer: 1000
+                    });
+                    $("#join-course").hide();
+                    $("#create-job").show();
+                    $("#create-job").attr("href", "candidate-dashboard.html#post-new-job#" + courseID);
+                }
+                setTimeout(function () {
+                    location.reload();
+                }, 1000);
+            },
+            error: function (xhr) {
+                swal(xhr.message, "Error!", 'error');
+            }
+        });
+    })
+})
--- src/main/webapp/assets/js/ajax/post-job.js
+++ src/main/webapp/assets/js/ajax/post-job.js
@@ -1,21 +1,40 @@
-$(function() {
-    $("#post-job-save-update").click(function(message) {
-        let $title = $("#job-title").val();
-        let $profile = $("#jb-description").summernote('code');
-        let $exceptedNumOfMember = $("#exceptedNumOfMember").val();
-        let $college = $("#category1").val();
-        let $campus = $("#category2").val();
-        let $telephone = $("#job-telephone").val();
-        let $emailID = $("#job-email").val();
-        let $expected_end_time = $("#expected-end-time").val();
+$(function () {
+    let ur = location.href;
+    let courseID = decodeURI(ur.split('#')[2]);
+    if (courseID !== '') {
+        $.ajax({
+            type: "get",
+            url: "http://localhost:8080/JianMu_war/job-post",
+            data: {"id": courseID},
+            dataType: "json",
+            async: false,
+            success: function (msg) {
+                $("#job-course").text(msg.courseTitle);
+            },
+            error: function (xhr) {
+                swal(xhr.message, "Error!", 'error');
+            }
+        });
+    }
+    $("#post-job-save-update").click(function (message) {
+        let title = $("#job-title").val();
+        let profile = $("#jb-description").summernote('code');
+        let exceptedNumOfMember = $("#exceptedNumOfMember").val();
+        let college = $("#category1").val();
+        let campus = $("#category2").val();
+        let telephone = $("#job-telephone").val();
+        let emailID = $("#job-email").val();
+        let expected_end_time = $("#expected-end-time").val();
+        let courseTitle = $("#job-course").val();
         let jobdata = {
-            "emailID": $emailID,
-            "title": $title,
-            "telephone": $telephone,
-            "profile": $profile,
-            "college": $college,
-            "campus": $campus,
-            "exceptedNumOfMember": $exceptedNumOfMember,
+            "emailID": emailID,
+            "title": title,
+            "telephone": telephone,
+            "profile": profile,
+            "college": college,
+            "campus": campus,
+            "exceptedNumOfMember": exceptedNumOfMember,
+            "courseTitle": courseTitle
         };
         jobdata = JSON.stringify(jobdata);
         $.ajax({
@@ -23,11 +42,11 @@ $(function() {
             url: "http://localhost:8080/JianMu_war/job_post",
             data: {
                 "jobdata": jobdata,
-                "expected_end_time": $expected_end_time
+                "expected_end_time": expected_end_time
             },
             dataType: "json",
             async: false,
-            success: function(msg) {
+            success: function (msg) {
                 swal({
                     title: msg.message,
                     text: "Success!",
@@ -35,7 +54,7 @@ $(function() {
                     timer: 1000
                 });
             },
-            error: function(xhr) {
+            error: function (xhr) {
                 swal(xhr.message, "Error!", 'error');
             }
         });
